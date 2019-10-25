9d03bfb421eda2906910620fd37734fc2ea0c768
Anderson Ferrari <aferrari@pathadmin.com>
2019-10-25 19:24:02 -0300

    final changes

--- MVCAPP/Startup.cs
+++ MVCAPP/Startup.cs
@@ -24,6 +24,7 @@ namespace MVCAPP
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            // !!!!!
             services.AddServerSideBlazor();
         }
 
